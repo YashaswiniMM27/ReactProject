99e735ab8d4ea4f6cbca1ceebd50fdc3abbfd92b
YashaswiniMM27 <yashaswinimm2703@gmail.com>
2025-01-04 22:28:32 +0530

    Static page using react

---
+++ src/navbar.jsx
@@ -0,0 +1,10 @@
+function Navbar(){
+return(
+    <nav className="reactNavbar">
+            <img className="reactLogo" src="/src/assets/React-icon.svg.png" alt="Image" />
+            <div className="reactFacts">ReactFacts</div>
+    </nav>
+)
+}
+
+export default Navbar
